f8879bd8fd67b4f982897e0df74e3593b69e4ca4
joankirui <joankirui99@gmail.com>
2021-11-06 22:08:13 +0300

    add classes user,pitch and comment

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from datetime import datetime
 
 
 
@@ -6,6 +7,43 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches_id'))
+
 
     def __repr__(self):
         return f'User {self.username}'
+
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String(255),nullable = False)
+    post = db.Column(db.Text(), nullable = False)
+    comment = db.relationship('Comment',backref='pitch',lazy='dynamic')
+    upvote = db.relationship('Upvote',backref='pitch',lazy='dynamic')
+    downvote = db.relationship('Downvote',backref='pitch',lazy='dynamic')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    time = db.Column(db.DateTime, default = datetime.utcnow)
+    category = db.Column(db.String(255), index = True,nullable = False)
+
+    def save_p(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Pitch {self.post}'
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer,Primary_key=True)
+    comment = db.Column(db.Text(),nullable = False)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id',nullable = False))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id',nullable = False))
+
+    def save_c(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'comment:{self.comment}'
+
--- manage.py
+++ manage.py
@@ -1,11 +1,13 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
+from app.models import User
 
 # Creating app instance 
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+
 @manager.command
 def test():
     '''
@@ -14,6 +16,10 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosit=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User)
 
 if __name__ == '__main__':
     manager.run()
