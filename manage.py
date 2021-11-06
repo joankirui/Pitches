194a423fd629d66360d3c4a3dd1fc1e4c386a9fe
joankirui <joankirui99@gmail.com>
2021-11-06 22:41:22 +0300

    import Pitch,Comment,Upvote and Downvote classes

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User
+from app.models import User,Pitch,Comment,Upvote,Downvote
 
 # Creating app instance 
 app = create_app('development')
@@ -19,7 +19,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User)
+    return dict(app = app,db = db,User = User,Pitch = Pitch,Comment = Comment,Upvote = Upvote, Downvote = Downvote )
 
 if __name__ == '__main__':
     manager.run()
