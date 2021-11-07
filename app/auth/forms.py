cac93f8bbe4914523c131819e550e11ae99f1e7d
joankirui <joankirui99@gmail.com>
2021-11-07 14:37:46 +0300

    create a login form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,8 +1,14 @@
 from flask_wtf import FlaskForm
 from flask_wtf.recaptcha import validators
-from wtforms import ValidationError,StringField,PasswordField,SubmitField
+from wtforms import ValidationError,StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators=[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
 
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators =[Required(),Email()])
